dde130a846c7994bd8432c4b7fcd3dceada3ba78
WagnerReis <wagnerreis1997@gmail.com>
2021-08-06 16:29:53 -0300

    Correção

--- src/App.js
+++ src/App.js
@@ -6,33 +6,33 @@ import "./styles.css";
 function App() {
   const [repositories, setRepositories] = useState([]);
 
-  async function handleAddRepository() {
-    const response = await api.post('repositories',{
-      title: `Dev React ${Date.now()}`,
-      url: "url.qualquer.com",
-      techs: "React",
-      likes: 0
-    });
-    const repositorie = response.data;
-
-    setRepositories([...repositories, repositorie]);
-  }
-
-  async function handleRemoveRepository(id) {
-    const responde = await api.delete(`repositories/${id}`);
-    return;
-  }
-
   useEffect(() => {
     api.get('repositories').then(response => {
       setRepositories(response.data);
     });
-  },[repositories]);
+  },[]);
 
+  async function handleAddRepository() {
+    const response = await api.post('repositories',{
+      title: `Dev React ${Date.now()}`,
+      url: "url.qualquer.com",
+      techs: "React"
+    });
+
+    setRepositories([...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repositorie => repositorie.id !== id
+    ))
+  }
+ 
   return (
     <div>
       <ul data-testid="repository-list">
-        <ul>
           {repositories.map(repositorie => 
             <li key={repositorie.id}>
                 {repositorie.title} 
@@ -40,7 +40,6 @@ function App() {
                 Remover
                 </button>
             </li>)}
-        </ul>
       </ul>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
